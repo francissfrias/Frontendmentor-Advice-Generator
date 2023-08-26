5cfd19452e5be1e1000ad261718f966418158a37
francissfrias <89100734+francissfrias@users.noreply.github.com>
2023-08-26 17:26:44 +0800

    DeploymentSteps

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(),svgr()],
+
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react(), svgr()],
+
+    base: '/',
+  };
+
+  if (command !== 'serve') {
+    config.base = '/Frontendmentor-Advice-Generator/';
+  }
+
+  return config;
 });
